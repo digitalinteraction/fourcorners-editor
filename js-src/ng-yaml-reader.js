f19f2134b3c15c10e3fed8ec39b599f0eca7e978
Tim Osadchiy <t.osadchiy@ncl.ac.uk>
2016-12-13 19:35:26 +0000

    ability to read from image

--- js-src/ng-yaml-reader.js
+++ js-src/ng-yaml-reader.js
@@ -4,64 +4,84 @@
 
 'use strict';
 
-var yaml = require('js-yaml'),
-    dataIsValid = require('./image-data-is-valid');
+var yaml = require("js-yaml"),
+    EXIF = require("exif-js"),
+    dataIsValid = require("./image-data-is-valid");
 
 module.exports = function (app) {
     app.directive('ngYamlReader', [serviceFun]);
+};
 
-    function serviceFun() {
+function serviceFun() {
 
-        function controller(scope, element, attributes) {
+    function controller(scope, element, attributes) {
 
-            if (!yaml.safeLoad) {
-                throw 'Callback onRead is not defined';
+        if (!yaml.safeLoad) {
+            throw 'Callback onRead is not defined';
+        }
+
+        var fileInput = element.find('input')[0];
+
+        // Display the directive only if the file api is supported
+        scope.visible = window.File && window.FileReader && window.FileList && window.Blob;
+        scope.errorList = [];
+
+        fileInput.addEventListener('change', handleFileSelect, false);
+
+        function handleFileSelect(evt) {
+            var file = fileInput.files[0];
+            if (!file) {
+                return;
             }
-
-            var fileInput = element.find('input')[0];
-
-            // Display the directive only if the file api is supported
-            scope.visible = window.File && window.FileReader && window.FileList && window.Blob;
-            scope.errorList = [];
-
-            fileInput.addEventListener('change', handleFileSelect, false);
-            function handleFileSelect(evt) {
-                var file = fileInput.files[0];
-                if (!file) {
-                    return;
-                }
-                var reader = new FileReader();
+            var reader = new FileReader();
+            if (!file.type.match(/image.*/)) {
                 reader.onload = function (e) {
-                    var data;
-
-                    scope.errorList.length = 0;
-
-                    try {
-                        data = yaml.safeLoad(reader.result);
-                        var errors = dataIsValid(data);
-                        scope.errorList.push.apply(scope.errorList, errors);
-                    } catch (e) {
-                        scope.errorList.push('File has incorrect structure: ' + e.message);
-                    }
-                    if (scope.errorList.length) {
-                        scope.$apply();
-                    } else {
-                        scope.onRead(data);
-                    }
+                    readFromYaml(e, reader);
                     fileInput.value = "";
                 };
                 reader.readAsText(file);
+            } else {
+                reader.onload = function (e) {
+                    readFromImage(e, file);
+                    fileInput.value = "";
+                };
+                reader.readAsDataURL(file);
             }
         }
 
-        return {
-            restrict: 'A',
-            link: controller,
-            scope: {
-                onRead: '='
-            },
-            templateUrl: 'ng-templates/ng-yaml-reader.html'
-        };
+        function readFromYaml(e, reader) {
+            var data;
+            scope.errorList.length = 0;
+            try {
+                data = yaml.safeLoad(reader.result);
+                var errors = dataIsValid(data);
+                scope.errorList.push.apply(scope.errorList, errors);
+            } catch (e) {
+                scope.errorList.push('File has incorrect structure: ' + e.message);
+            }
+            if (scope.errorList.length) {
+                scope.$apply();
+            } else {
+                scope.onRead(data);
+            }
+        }
+
+        function readFromImage(e, file) {
+            EXIF.getData(file, function() {
+                var allMetaData = EXIF.getAllTags(this);
+                // Perform some actions with allMetaData
+                //console.log(allMetaData);
+            })
+        }
+
     }
 
-};
+    return {
+        restrict: 'A',
+        link: controller,
+        scope: {
+            onRead: '='
+        },
+        templateUrl: 'ng-templates/ng-yaml-reader.html'
+    };
+}
