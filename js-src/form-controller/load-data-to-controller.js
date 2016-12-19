86a018873c98767c657f2c08637b8f1b9858e791
Tim Osadchiy <t.osadchiy@ncl.ac.uk>
2016-12-19 17:35:40 +0000

    ignore missing fields

--- js-src/form-controller/load-data-to-controller.js
+++ js-src/form-controller/load-data-to-controller.js
@@ -8,6 +8,9 @@ var ContextSourceModel = require("./context-source-model"),
     LinkModel = require("./link-model");
 
 module.exports = function (data, appConstants) {
+
+    completeData(data);
+
     this.contextSources = data.context.map(function (c) {
         return new ContextSourceModel(appConstants, {
             sourceType: c.src ? appConstants.SOURCE_TYPES[0] : appConstants.SOURCE_TYPES[1],
@@ -31,13 +34,26 @@ module.exports = function (data, appConstants) {
         url: data.backStory.magazineUrl
     };
 
-    var copyright = data.creativeCommons.copyright.split(' © ');
+    var copyright = data.creativeCommons.copyright ? data.creativeCommons.copyright.split(/ ©( |)/) : ["", ""];
     this.creativeCommons = {
-        ccOwnerName: copyright[0],
-        ccYear: copyright[1],
+        ccOwnerName: copyright[0].replace(/^\s\s*/, "").replace(/\s\s*$/, ""),
+        ccYear: copyright[1].replace(/^\s\s*/, "").replace(/\s\s*$/, ""),
         codeOfEthics: data.creativeCommons.codeOfEthics,
         description: data.creativeCommons.description
     };
     this.toggleView();
     this.$apply();
 };
+
+function completeData(data) {
+    data.context = data.context || [];
+    data.links = data.links || [];
+    data.context = data.context || [];
+    data.links = data.links || [];
+    data.backStory = data.backStory || {};
+    data.backStory = data.backStory || {};
+    data.creativeCommons = data.creativeCommons || {};
+    if (data.creativeCommons.copyright) {
+        data.creativeCommons.copyright = data.creativeCommons.copyright.replace("(c)", "©")
+    }
+}
